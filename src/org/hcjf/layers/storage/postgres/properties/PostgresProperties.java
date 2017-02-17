c93a4ebc2841cf5cefe2b63b9432938cbdd52c47
kevin.winter <kevin.winter@sitrack.com>
2017-02-17 13:21:55 -0300

    ISSUE: #0020758: Sitrack Auth service SOLUTION: is null - is not null

--- src/org/hcjf/layers/storage/postgres/properties/PostgresProperties.java
+++ src/org/hcjf/layers/storage/postgres/properties/PostgresProperties.java
@@ -18,6 +18,8 @@ public final class PostgresProperties {
         public static final String GRATER_THAN_OPERATOR = "postgres.storage.layer.greater.than.operator";
         public static final String SMALLER_THAN_OPERATOR = "postgres.storage.layer.smaller.than.operator";
         public static final String LIKE_OPERATOR = "postgres.storage.layer.like.operator";
+        public static final String IS_NULL_OPERATOR = "postgres.storage.layer.is.null.operator";
+        public static final String IS_NOT_NULL_OPERATOR = "postgres.storage.layer.is.not.null.operator";
 
     }
 
@@ -25,6 +27,8 @@ public final class PostgresProperties {
         SystemProperties.putDefaultValue(POSTGRES_STORAGE_LAYER_LOG_TAG, "Postgres");
 
         SystemProperties.putDefaultValue(ReservedWord.LIKE_OPERATOR, "LIKE");
+        SystemProperties.putDefaultValue(ReservedWord.IS_NULL_OPERATOR, "IS NULL");
+        SystemProperties.putDefaultValue(ReservedWord.IS_NOT_NULL_OPERATOR, "IS NOT NULL");
     }
 
 }
